fd60d9bb88a8255c1ade577b427555252d8473ad
smart04240 <smart04240@gmail.com>
2021-04-09 04:09:14 +1000

    fix style

--- dist/app.c61986b1.js
+++ dist/app.c61986b1.js
@@ -3503,11 +3503,6 @@ var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
   }
 };
 
-var originalWidth = 2970;
-var originalHeight = 2370;
-var huecoAhueco = 330;
-var huecoAhuecoAbajo = 225;
-var originalDown = 660;
 var y = 0;
 
 var Board = function (_super) {
@@ -5254,7 +5249,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63833" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50285" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
